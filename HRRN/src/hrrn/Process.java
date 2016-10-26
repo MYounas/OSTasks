a5e0af9b27482b58a549b182703a860f989b4182
MYounas <younus.ubit56@gmail.com>
2016-10-26 09:34:27 +0500

    HRRN init

---
+++ HRRN/src/hrrn/Process.java
@@ -0,0 +1,22 @@
+package hrrn;
+
+
+public class Process {
+
+    int id,ex,ar,t_time,wt_time;float ut_time,res_ratio;
+    
+    public Process(int id,int e,int a) {
+        this.id=id;
+        ex=e;
+        ar=a;
+        res_ratio=(HRRN.last_finish+ex)/ex;
+    }
+    
+    void runProcess(){
+        HRRN.last_finish+=ex;
+        t_time=HRRN.last_finish-ar;
+        wt_time=t_time-ex;
+        ut_time=((float)ex/(float)t_time)*100;
+    }
+    
+}
