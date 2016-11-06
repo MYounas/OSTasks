5df0d6c12f26eb7b5913aaa8aa954a39bdb27dc9
MYounas <younus.ubit56@gmail.com>
2016-11-06 14:57:06 +0500

    almost done sjf

--- SJF/src/sjf/Process.java
+++ SJF/src/sjf/Process.java
@@ -5,15 +5,17 @@
  */
 package sjf;
 
+
 /**
  *
  * @author MY
  */
 public class Process {
 
-    int id,ex,ar,t_time,wt_time;float ut_time;
+    int finish_time,id,ex,ar,t_time,wt_time,colorVal;float ut_time;
     
     public Process(int id,int e,int a) {
+        colorVal=30+id;
         this.id=id;
         ex=e;
         ar=a;
@@ -21,6 +23,7 @@ public class Process {
     
     void runProcess(){
         SJF.last_finish+=ex;
+        finish_time=SJF.last_finish;
         t_time=SJF.last_finish-ar;
         wt_time=t_time-ex;
         ut_time=((float)ex/(float)t_time)*100;
--- SJF/src/sjf/SJF.java
+++ SJF/src/sjf/SJF.java
@@ -24,17 +24,14 @@ public class SJF {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        
-        
         Scanner in = new Scanner(System.in);
         int n, i, j, k,simpleRun=0, e, a;
-        for(i=0;i<10;i++){
-            System.out.print(i+"--");
-        }
+        
         System.out.print("Enter no of Processes:");
         n = in.nextInt();
         Process p[] = new Process[n];
         ArrayList<Process> proc=new ArrayList<>();
+        ArrayList<Process> colorProc=new ArrayList<>();
         for (i = 0; i < n; i++) {
             System.out.print("Execution Time of Process" + (i + 1) + " :");
             e = in.nextInt();
@@ -44,7 +41,9 @@ public class SJF {
             proc.add(p[i]);
         }
         ArrayList<Process> list = new ArrayList();Process tempRun;
+        colorProc.add(p[0]);
         p[simpleRun++].runProcess();
+        
         
         for (i = 0; i < (n - 1); i++) {
             if (flag == 0) {
@@ -54,11 +53,11 @@ public class SJF {
                     }
                 }
                 
-                if(list.isEmpty()){
-                    list.add(proc.get(simpleRun));
-                    list.remove(proc.get(simpleRun++));
-                }
-                else{
+//                if(list.isEmpty()){
+//                    (proc.get(simpleRun)).runProcess();
+//                    proc.remove(simpleRun++));
+//                }
+//                else{
                     Collections.sort((List)list,new Comparator<Process>() {
                     @Override
                     public int compare(Process o1, Process o2) {
@@ -69,10 +68,11 @@ public class SJF {
                     }
                 });
                 tempRun = list.get(0);
+                colorProc.add(tempRun);
                 tempRun.runProcess();
                 proc.remove(tempRun);
                 list.remove(tempRun);
-                }
+//                }
                 
             }
             flag = (list.size() == (n - simpleRun-1)) ? 1 : 0;
@@ -80,6 +80,7 @@ public class SJF {
             if(flag==1){
                 do{
                     tempRun=list.get(0);
+                    colorProc.add(tempRun);
                     tempRun.runProcess();
                     proc.remove(tempRun);
                     list.remove(tempRun);
@@ -90,11 +91,48 @@ public class SJF {
             
         }
         
+        System.out.println("\n\n\n");
+        
+        for(i=0;i<=last_finish*6;i++){
+            System.out.print("_");
+        }
+        
+        System.out.println("");
+        for(i=0;i<=last_finish;i++){
+            System.out.print(i);
+            for(j=0;j<4;j++){
+                System.out.print(" ");
+            }
+        }
+        
+        System.out.println("");
+//        for(i=0;i<=last_finish;i++){
+//            for(j=0;j<4;j++){
+//                System.out.print(" ");
+//            }
+//            System.out.print("|");
+//        }
+        
+        System.out.println();StringBuilder sbCount=new StringBuilder();
+        StringBuilder sbProc=new StringBuilder();
+        Process curP;j=0;
+        for(i=0;i<n;i++){
+            curP=colorProc.get(i);
+            for(;j<=last_finish;j++){
+                if(j==curP.finish_time){
+                    System.out.print((char)27+"["+curP.colorVal+"mP["+curP.id+"]");
+//                    System.out.print((char)27+"["+curP.colorVal+"m"+curP.id);
+                    break;
+                }
+                System.out.print("     ");
+            }
+        }
         
         
-        System.out.println("-------------------------------------------");
+        System.out.println("\n\n\n");
+        System.out.println("----------------------------------------------");
         System.out.println("Pro  |  T.T  |  wait Time  |  Utilization Time");
-        System.out.println("-------------------------------------------");
+        System.out.println("-----------------------------------------------");
         for (i = 0; i < n; i++) {
             System.out.println("P" + p[i].id + "  |  " + p[i].t_time + "  |  " + p[i].wt_time + "  |  " + p[i].ut_time + "%");
         }
